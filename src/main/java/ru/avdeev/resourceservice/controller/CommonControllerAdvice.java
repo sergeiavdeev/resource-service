7f545b294463e109f23d7f51e872f0b85ac63990
serg <avdey1978@gmail.com>
2023-09-07 09:43:04 +0300

    Refactor

--- src/main/java/ru/avdeev/resourceservice/controller/CommonControllerAdvice.java
+++ src/main/java/ru/avdeev/resourceservice/controller/CommonControllerAdvice.java
@@ -1,17 +1,13 @@
 package ru.avdeev.resourceservice.controller;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 import ru.avdeev.resourceservice.dto.Error;
 import ru.avdeev.resourceservice.exception.ResourceNotFoundException;
 
 @ControllerAdvice
-public class StorageControllerAdvice {
+public class CommonControllerAdvice {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     ResponseEntity<Error> resourceNotFond(ResourceNotFoundException ex) {
